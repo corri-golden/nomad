b11c819029474bd8f03a95c0359d7ace98dcd746
Corri Golden <corri.golden@gmail.com>
2020-12-21 13:04:00 -0600

    upload photo admin

--- nomadproject/urls.py
+++ nomadproject/urls.py
@@ -17,8 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from nomadapp.models import *
 
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('nomadapp.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
